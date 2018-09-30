0039d31c8eaeadcce1783af64987bd5ede21f204
Mottaqui Karim <mottaquikarim@users.noreply.github.com>
2018-09-30 15:42:15 -0400

    Create pset5_declarations_vs_expressions.js

---
+++ pset5_declarations_vs_expressions.js
@@ -0,0 +1,32 @@
+/*
+    FOR EACH PROBLEM BELOW, IMPLEMENT THREE WAYS
+    1. AS A FUNCTION DECLARATION
+    2. AS AN ANONYMOUS FUNCTION EXPRESSION
+    3. AS A NAMED FUNCTION EXPRESSION
+    
+    RUN BOTH TO ENSURE THEY WORK PROPERLY
+*/
+
+
+/*
+    PROBLEM 1:
+        Write a function that takes THREE params: a, b, c
+        return the remainder of the sum of a,b,c divided by 3
+*/ 
+
+
+/*
+    PROBLEM 2:
+        Write a function that takes THREE params: a,b,c
+        return a string that addes a dash in between them.
+        
+        so, if a = 'one', b = 'two', c='three'
+        then function will return 'one-two-three'
+*/
+
+/*
+    PROBLEM 3:
+        Write a function that takes NO params
+        return a random number between 0 and 10 
+        (doesn't have to be a whole number tho)
+*/
